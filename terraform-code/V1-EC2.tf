11d4ac3e79e37390e913048231f95fc0332eb7de
kannanvelu <kannan80500@gmail.com>
2023-10-10 07:49:08 +0530

    commiting V1-EC2.tf

---
+++ terraform-code/V1-EC2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-0bb4c991fa89d4b9b"
+    instance_type = "t2.micro"
+    key_name = "devkey"
+  
+}
