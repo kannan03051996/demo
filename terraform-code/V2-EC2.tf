06c3fe2b59c04a62fb5e197347c542d75f2a9a00
kannanvelu <kannan80500@gmail.com>
2023-10-10 08:10:01 +0530

    adding V2 Version

---
+++ terraform-code/V2-EC2.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-0bb4c991fa89d4b9b"
+    instance_type = "t2.micro"
+    key_name = "devkey"
+    security_groups = ["demo-sg"] 
+  
+}
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+ 
+  ingress {
+    description      = "SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "SSH Port"
+  }
+}
